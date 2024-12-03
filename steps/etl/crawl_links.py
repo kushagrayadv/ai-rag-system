076884656a3759a0bfd48ee13fb8e4a54c626910
Sohith Bandari <sohith.bandari@gmail.com>
2024-12-03 10:19:09 -0500

    fixed crawler_link after push

--- steps/etl/crawl_links.py
+++ steps/etl/crawl_links.py
@@ -11,41 +11,43 @@ from domain.documents import UserDocument
 
 
 def crawl_links(user: UserDocument, links: List[str]) -> List[str]:
-  
-  links = json.loads(links)
 
-  def _crawl_link(dispatcher: CrawlerDispatcher, link: str, user: UserDocument) -> tuple[bool, str]:
-    crawler = dispatcher.get_crawler(link)
-    crawler_domain = urlparse(link).netloc
+    links = json.loads(links)
 
-  def _add_to_metadata(metadata: Dict, domain: str, successful_crawl: bool) -> dict:
-    if domain not in metadata:
-      metadata[domain] = {"successful": 0, "total": 0}
-    metadata[domain]["successful"] += int(successful_crawl)
-    metadata[domain]["total"] += 1
-    return metadata
+    def _crawl_link(dispatcher: CrawlerDispatcher, link: str, user: UserDocument) -> tuple[bool, str]:
+        crawler = dispatcher.get_crawler(link)
+        crawler_domain = urlparse(link).netloc
 
-  dispatcher = CrawlerDispatcher.build().register_github().register_youtube()
+        try:
+            crawler.extract(link=link, user=user)
+            return True, crawler_domain
+        except Exception as e:
+            logger.error(f"An error occurred while crawling: {e!s}")
+            return False, crawler_domain
 
-  logger.info(f"Starting to crawl {len(links)} link(s).")
 
-  metadata = {}
-  successful_crawls = 0
-  failed_crawled_links = []
-  for link in tqdm(links):
-    successful_crawl, crawled_domain = _crawl_link(dispatcher, link, user)
-    successful_crawls += successful_crawl
+    def _add_to_metadata(metadata: Dict, domain: str, successful_crawl: bool) -> dict:
+        if domain not in metadata:
+            metadata[domain] = {"successful": 0, "total": 0}
+        metadata[domain]["successful"] += int(successful_crawl)
+        metadata[domain]["total"] += 1
+        return metadata
 
-    if not successful_crawl:
-      failed_crawled_links.append(crawled_domain)
+    dispatcher = CrawlerDispatcher.build().register_github().register_youtube()
 
-    metadata = _add_to_metadata(metadata, crawled_domain, successful_crawl)
+    logger.info(f"Starting to crawl {len(links)} link(s).")
 
-  metadata["failed_crawled_links"] = failed_crawled_links
+    metadata = {}
+    successful_crawls = 0
+    for link in tqdm(links):
+        successful_crawl, crawled_domain = _crawl_link(dispatcher, link, user)
+        successful_crawls += successful_crawl
 
-  task = Task.current_task()
-  task.upload_artifact("crawled_links_metadata", metadata)
+        metadata = _add_to_metadata(metadata, crawled_domain, successful_crawl)
 
-  logger.info(f"Successfully crawled {successful_crawls} / {len(links)} links.")
+    task = Task.current_task()
+    task.upload_artifact("crawled_links", metadata)
 
-  return links
+    logger.info(f"Successfully crawled {successful_crawls} / {len(links)} links.")
+
+    return links
