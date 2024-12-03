24ba6e88ef5d4bb9fc8cf348f09556720667f656
Sohith Bandari <sohith.bandari@gmail.com>
2024-12-02 21:30:05 -0500

    fixed step 1

--- steps/etl/get_or_create_user.py
+++ steps/etl/get_or_create_user.py
@@ -5,17 +5,6 @@ from typing import Dict
 from application import utils
 from domain.documents import UserDocument
 
-def _get_metadata(user_full_name: str, user: UserDocument) -> Dict:
-    return {
-        "query": {
-            "user_full_name": user_full_name,
-        },
-        "retrieved": {
-            "user_id": str(user.id),
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-        },
-    }
 
 def get_or_create_user(user_full_name: str) -> UserDocument:
 
@@ -23,10 +12,19 @@ def get_or_create_user(user_full_name: str) -> UserDocument:
 
     first_name, last_name = utils.split_user_full_name(user_full_name)
 
-    user = UserDocument.get_or_create(first_name=first_name, last_name=last_name)
+    user = UserDocument.get_or_create(first_name=first_name, last_name=last_name) 
 
     task = Task.current_task()
-    metadata = _get_metadata(user_full_name, user)
+    metadata = {
+            "query": {
+                "user_full_name": user_full_name,
+            },
+            "retrieved": {
+                "user_id": str(user.id),
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+            },
+        }
     task.upload_artifact("user_metadata", metadata)
 
     return user
