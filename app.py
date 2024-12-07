379fc9d5a8036235745d06505f5ed810c15768b9
Kushagra Yadav <kushagrayadv15@gmail.com>
2024-12-06 20:35:32 -0500

    inference pipeline changes

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+import gradio as gr
+
 from infrastructure.inference_pipeline import rag
 
-if __name__ == '__main__':
-  query = """
-    My name is Admin User.
-    I want to know what is ros2 ?
-  """
+prepopulated_questions = [
+  "",
+  "Tell me how can I navigate to a specific pose - include replanning aspects in your answer.",
+  "Can you provide me with code for this task?"
+]
 
-  answer = rag(query)
+
+def get_response(selected_question):
+  response = ""
+  answer_stream = rag(selected_question)
+
+  # Update the response in chunks
+  for chunk in answer_stream:
+    response += chunk['message']['content']
+    yield response  # Stream the output live
+
+
+gradio_app = gr.Interface(
+  fn=get_response,
+  inputs=gr.Dropdown(choices=prepopulated_questions, label="Select Your Question"),
+  outputs=gr.Textbox(label="Answer", interactive=True),
+  title="AI RAG System",
+  description="Interact with the AI RAG System. Select a question from the dropdown and get a live answer.",
+  live=True
+)
+
+if __name__ == '__main__':
+  gradio_app.launch()
